5c37253443d041e96703e4eafcd73268ebf502b4
Yannis Gravezas <wizgrav@gmail.com>
2024-02-19 00:46:58 +0200

    build

--- build/three.module.js
+++ build/three.module.js
@@ -13894,7 +13894,7 @@ var morphcolor_vertex = "#if defined( USE_MORPHCOLORS ) && defined( MORPHTARGETS
 
 var morphnormal_vertex = "#ifdef USE_MORPHNORMALS\n\tobjectNormal *= morphTargetBaseInfluence;\n\t#ifdef MORPHTARGETS_TEXTURE\n\t\tfor ( int i = 0; i < MORPHTARGETS_COUNT; i ++ ) {\n\t\t\tif ( morphTargetInfluences[ i ] != 0.0 ) objectNormal += getMorph( gl_VertexID, i, 1 ).xyz * morphTargetInfluences[ i ];\n\t\t}\n\t#else\n\t\tobjectNormal += morphNormal0 * morphTargetInfluences[ 0 ];\n\t\tobjectNormal += morphNormal1 * morphTargetInfluences[ 1 ];\n\t\tobjectNormal += morphNormal2 * morphTargetInfluences[ 2 ];\n\t\tobjectNormal += morphNormal3 * morphTargetInfluences[ 3 ];\n\t#endif\n#endif";
 
-var morphtarget_pars_vertex = "#ifdef USE_MORPHTARGETS\n\t#ifndef USE_INSTANCING_MORPH\n\t\tuniform float morphTargetBaseInfluence;\n\t#endif\n\t#ifdef MORPHTARGETS_TEXTURE\n\t\t#ifndef USE_INSTANCING_MORPH\n\t\t\tuniform float morphTargetInfluences[ MORPHTARGETS_COUNT ];\n\t\t#endif\n\t\tuniform sampler2DArray morphTargetsTexture;\n\t\tuniform ivec2 morphTargetsTextureSize;\n\t\tvec4 getMorph( const in int vertexIndex, const in int morphTargetIndex, const in int offset ) {\n\t\t\tint texelIndex = vertexIndex * MORPHTARGETS_TEXTURE_STRIDE + offset;\n\t\t\tint y = texelIndex / morphTargetsTextureSize.x;\n\t\t\tint x = texelIndex - y * morphTargetsTextureSize.x;\n\t\t\tivec3 morphUV = ivec3( x, y, morphTargetIndex );\n\t\t\treturn texelFetch( morphTargetsTexture, morphUV, 0 );\n\t\t}\n\t#else\n\t\t#ifndef USE_MORPHNORMALS\n\t\t\tuniform float morphTargetInfluences[ 8 ];\n\t\t#else\n\t\t\tuniform float morphTargetInfluences[ 4 ];\n\t\t#endif\n\t#endif\n#endif";
+var morphtarget_pars_vertex = "#ifdef USE_MORPHTARGETS\n\t#ifndef USE_INSTANCING_MORPH\n\t\tuniform float morphTargetBaseInfluence;\n\t#endif\n\t#ifdef MORPHTARGETS_TEXTURE\n\t\t#ifndef USE_INSTANCING_MORPH\n\t\t\tuniform float morphTargetInfluences[ MORPHTARGETS_COUNT ];\n\t\t#endif\n\t\tuniform sampler2DArray morphTargetsTexture;\n\t\tuniform ivec2 morphTargetsTextureSize;\n\t\tvec4 getMorph( const in int vertexIndex, const in int morphTargetIndex, const in int offset ) {\n\t\t\tint texelIndex = vertexIndex * MORPHTARGETS_TEXTURE_STRIDE + 3 * offset;\n\t\t\tint y = texelIndex / morphTargetsTextureSize.x;\n\t\t\tint x = texelIndex - y * morphTargetsTextureSize.x;\n\t\t\tivec3 morphUV = ivec3( x, y, morphTargetIndex );\n\t\t\tvec4 ret = vec4(0.);\n\t\t\tret.x = texelFetch( morphTargetsTexture, morphUV, 0 ).r;\n\t\t\tmorphUV.x++;\n\t\t\tret.y = texelFetch( morphTargetsTexture, morphUV, 0 ).r;\n\t\t\tmorphUV.x++;\n\t\t\tret.z = texelFetch( morphTargetsTexture, morphUV, 0 ).r;\n\t\t\t#if MORPHTARGETS_TEXTURE_STRIDE == 10\n\t\t\t\tmorphUV.x++;\n\t\t\t\tret.a = offset == 2 ? texelFetch( morphTargetsTexture, morphUV, 0 ).r : 0.;\n\t\t\t#else\n\t\t\t\tret.a = 0.;\n\t\t\t#endif\n\t\t\treturn ret;\n\t\t}\n\t#else\n\t\t#ifndef USE_MORPHNORMALS\n\t\t\tuniform float morphTargetInfluences[ 8 ];\n\t\t#else\n\t\t\tuniform float morphTargetInfluences[ 4 ];\n\t\t#endif\n\t#endif\n#endif";
 
 var morphtarget_vertex = "#ifdef USE_MORPHTARGETS\n\ttransformed *= morphTargetBaseInfluence;\n\t#ifdef MORPHTARGETS_TEXTURE\n\t\tfor ( int i = 0; i < MORPHTARGETS_COUNT; i ++ ) {\n\t\t\tif ( morphTargetInfluences[ i ] != 0.0 ) transformed += getMorph( gl_VertexID, i, 0 ).xyz * morphTargetInfluences[ i ];\n\t\t}\n\t#else\n\t\ttransformed += morphTarget0 * morphTargetInfluences[ 0 ];\n\t\ttransformed += morphTarget1 * morphTargetInfluences[ 1 ];\n\t\ttransformed += morphTarget2 * morphTargetInfluences[ 2 ];\n\t\ttransformed += morphTarget3 * morphTargetInfluences[ 3 ];\n\t\t#ifndef USE_MORPHNORMALS\n\t\t\ttransformed += morphTarget4 * morphTargetInfluences[ 4 ];\n\t\t\ttransformed += morphTarget5 * morphTargetInfluences[ 5 ];\n\t\t\ttransformed += morphTarget6 * morphTargetInfluences[ 6 ];\n\t\t\ttransformed += morphTarget7 * morphTargetInfluences[ 7 ];\n\t\t#endif\n\t#endif\n#endif";
 
@@ -17745,29 +17745,30 @@ function WebGLMorphtargets( gl, capabilities, textures ) {
 
 				let vertexDataCount = 0;
 
-				if ( hasMorphPosition === true ) vertexDataCount = 1;
-				if ( hasMorphNormals === true ) vertexDataCount = 2;
-				if ( hasMorphColors === true ) vertexDataCount = 3;
+				if ( hasMorphPosition === true ) vertexDataCount = 3;
+				if ( hasMorphNormals === true ) vertexDataCount = 6;
+				if ( hasMorphColors === true ) vertexDataCount = 10;
 
 				let width = geometry.attributes.position.count * vertexDataCount;
 				let height = 1;
 
 				if ( width > capabilities.maxTextureSize ) {
 
-					height = Math.ceil( width / capabilities.maxTextureSize );
+					// Align on stride to simplify the texel fetching in the shader
+					const strideWidth = Math.ceil( width / vertexDataCount ) * vertexDataCount;
+					height = Math.ceil( strideWidth / capabilities.maxTextureSize );
 					width = capabilities.maxTextureSize;
 
 				}
 
-				const buffer = new Float32Array( width * height * 4 * morphTargetsCount );
+				const buffer = new Float32Array( width * height * morphTargetsCount );
 
 				const texture = new DataArrayTexture( buffer, width, height, morphTargetsCount );
 				texture.type = FloatType;
+				texture.format = RedFormat;
 				texture.needsUpdate = true;
 
 				// fill buffer
-
-				const vertexDataStride = vertexDataCount * 4;
 
 				for ( let i = 0; i < morphTargetsCount; i ++ ) {
 
@@ -17775,11 +17776,11 @@ function WebGLMorphtargets( gl, capabilities, textures ) {
 					const morphNormal = morphNormals[ i ];
 					const morphColor = morphColors[ i ];
 
-					const offset = width * height * 4 * i;
+					const offset = width * height * i;
 
 					for ( let j = 0; j < morphTarget.count; j ++ ) {
 
-						const stride = j * vertexDataStride;
+						const stride = j * vertexDataCount;
 
 						if ( hasMorphPosition === true ) {
 
@@ -17788,7 +17789,6 @@ function WebGLMorphtargets( gl, capabilities, textures ) {
 							buffer[ offset + stride + 0 ] = morph.x;
 							buffer[ offset + stride + 1 ] = morph.y;
 							buffer[ offset + stride + 2 ] = morph.z;
-							buffer[ offset + stride + 3 ] = 0;
 
 						}
 
@@ -17796,10 +17796,9 @@ function WebGLMorphtargets( gl, capabilities, textures ) {
 
 							morph.fromBufferAttribute( morphNormal, j );
 
-							buffer[ offset + stride + 4 ] = morph.x;
-							buffer[ offset + stride + 5 ] = morph.y;
-							buffer[ offset + stride + 6 ] = morph.z;
-							buffer[ offset + stride + 7 ] = 0;
+							buffer[ offset + stride + 3 ] = morph.x;
+							buffer[ offset + stride + 4 ] = morph.y;
+							buffer[ offset + stride + 5 ] = morph.z;
 
 						}
 
@@ -17807,10 +17806,10 @@ function WebGLMorphtargets( gl, capabilities, textures ) {
 
 							morph.fromBufferAttribute( morphColor, j );
 
-							buffer[ offset + stride + 8 ] = morph.x;
-							buffer[ offset + stride + 9 ] = morph.y;
-							buffer[ offset + stride + 10 ] = morph.z;
-							buffer[ offset + stride + 11 ] = ( morphColor.itemSize === 4 ) ? morph.w : 1;
+							buffer[ offset + stride + 6 ] = morph.x;
+							buffer[ offset + stride + 7 ] = morph.y;
+							buffer[ offset + stride + 8 ] = morph.z;
+							buffer[ offset + stride + 9 ] = ( morphColor.itemSize === 4 ) ? morph.w : 1;
 
 						}
 
@@ -20604,9 +20603,9 @@ function WebGLPrograms( renderer, cubemaps, cubeuvmaps, extensions, capabilities
 
 		let morphTextureStride = 0;
 
-		if ( geometry.morphAttributes.position !== undefined ) morphTextureStride = 1;
-		if ( geometry.morphAttributes.normal !== undefined ) morphTextureStride = 2;
-		if ( geometry.morphAttributes.color !== undefined ) morphTextureStride = 3;
+		if ( geometry.morphAttributes.position !== undefined ) morphTextureStride = 3;
+		if ( geometry.morphAttributes.normal !== undefined ) morphTextureStride = 6;
+		if ( geometry.morphAttributes.color !== undefined ) morphTextureStride = 10;
 
 		//
 
